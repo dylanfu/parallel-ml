a041472a61fe76933e0f61fb2f57a2890d3cef26
Dylan Fu <dylan.fu@hotmail.co.nz>
2019-05-22 00:56:06 +1200

    Renamed test.py to mnist.py

--- mnist.py
+++ mnist.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 fashion_mnist = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images,
                                test_labels) = fashion_mnist.load_data()
