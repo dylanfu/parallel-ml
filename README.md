ea6197e5ff3ea4bd587338959b517d15fc608e18
Dylan Fu <dylan.fu@hotmail.co.nz>
2019-05-21 21:42:52 +1200

    Added flower.py script

--- README.md
+++ README.md
@@ -1,36 +1,60 @@
-# parallel-ml
+# Parallel Machine Learning
 
-# Installation
+# Installation and Setup
 
 ## Prerequisites
-- Python 3.4 +
+- Python 3.4 - 3.6 (Recommend Python 3.6.5_1)
 - Pip3
 - Virtualenv
 
+## Setup virtualenv
+Create virtual environment:
+Mac/Linux: `virtualenv --system-site-packages -p python3 ./venv`
+Windows: `virtualenv --system-site-packages -p python3 ./venv`
+
+Activate virtual environment (Must activate before running any python scripts):
+Mac/Linux: `source ./venv/bin/activate`
+Windows: `.\venv\Scripts\activate`
+
 ## Modules
 Install the following modules:
-- `pip install tensorflow`
+- `pip install six`
+- `pip install wrapt`
+- `pip install -U --pre tensorflow` contains the Tensorflow 2.0 Alpha release
+- `pip install tfds-nightly`
 - `pip install matplotlib`
 
 For MacOS Mojave
 - `pip install pyqt5`
 - Add ``matplotlib.use('Qt5Agg')`` in the script or create a ~/.matplotlib/matplotlibrc file containing ``backend: Qt5Agg``
 
-# Run the training script
-Run `python test.py`
+# Run scripts
+Activate virtual environment:
+- Mac/Linux: `source ./venv/bin/activate`
+- Windows: `.\venv\Scripts\activate`
 
-# Sequential Results (Torrance)
+## Train Models
+- To train MNIST dataset, run `python test.py`
+- To train Tensorflow's Flower dataset, run `python flower.py`
 
-## Specs
+Deactivate virtual environment when done by: `deactivate`
+
+# Sequential Results 
+
+## Torrance
+
+### Specs
 - Dell Inc. Inspiron 5567
 - i7-7500U CPU @ 2.70GHz
-- 2901 Mhz
+- Turbo Boost up to 3.50GHz
 - 2 Cores
 - 4 Logical Processors (Hyperthreading)
 
-## Results
+### Results
 
-| Iteration | Time (s)            | Accuracy  |
+#### Results for `test.py`
+
+| Epoch     | Time (s)            | Accuracy  |
 | --------- | ------------------- | --------  |
 | 1         | 18.626782417297363  | 0.8822    |
 | 2         | 17.893579721450806  | 0.877     |
@@ -38,3 +62,36 @@ Run `python test.py`
 | 4         | 17.704848051071167  | 0.8725    |
 | 5         | 16.981231212615967  | 0.8687    |
 | Average   | 17.754655647277833  | 0.8754    |
+
+## Dylan
+
+### Specs
+- Macbook Pro 15 2018
+- i7-8850H CPU @ 2.60GHz
+- Turbo Boost up to 4.30GHz
+- 6 Cores
+- 12 Logical Processors (Hyperthreading)
+
+### Results
+
+#### Results for `test.py`
+
+| Epoch     | Time (s)            | Accuracy  |
+| --------- | ------------------- | --------  |
+| 1         | ~                   | ~         |
+| 2         | ~                   | ~         |
+| 3         | ~                   | ~         |
+| 4         | ~                   | ~         |
+| 5         | ~                   | ~         |
+| Average   | ~                   | ~         |
+
+#### Results for `flower.py`
+
+| Epoch     | Time (s)            | Accuracy  |
+| --------- | ------------------- | --------  |
+| 1         | ~                   | ~         |
+| 2         | ~                   | ~         |
+| 3         | ~                   | ~         |
+| 4         | ~                   | ~         |
+| 5         | ~                   | ~         |
+| Average   | ~                   | ~         |
---
+++ flower.py
@@ -0,0 +1,121 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+EPOCHS = 5
+IMG_SIZE = 64
+IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
+SHUFFLE_BUFFER_SIZE = 1024
+BATCH_SIZE = 32
+
+"""
+Format given dataset
+"""
+def format_example(image, label):
+    image = tf.cast(image, tf.float32)
+    # Normalize the pixel values
+    image = image / 255.0
+    # Resize the image
+    image = tf.image.resize(image, (IMG_SIZE, IMG_SIZE))
+    return image, label
+
+"""
+Augments given dataset
+"""
+def augment_data(image, label):
+  print("Augment data called!")
+  image = tf.image.random_flip_left_right(image)
+  image = tf.image.random_contrast(image, lower=0.0, upper=1.0)
+  # Maybe add more augmentation
+  return image, label
+
+"""
+Creating a simple CNN model in keras using functional API
+"""
+def create_model():
+    img_inputs = keras.Input(shape=IMG_SHAPE)
+    conv_1 = keras.layers.Conv2D(32, (3, 3), activation='relu')(img_inputs)
+    maxpool_1 = keras.layers.MaxPooling2D((2, 2))(conv_1)
+    conv_2 = keras.layers.Conv2D(64, (3, 3), activation='relu')(maxpool_1)
+    maxpool_2 = keras.layers.MaxPooling2D((2, 2))(conv_2)
+    conv_3 = keras.layers.Conv2D(64, (3, 3), activation='relu')(maxpool_2)
+    flatten = keras.layers.Flatten()(conv_3)
+    dense_1 = keras.layers.Dense(64, activation='relu')(flatten)
+    output = keras.layers.Dense(metadata.features['label'].num_classes, activation='softmax')(dense_1)
+
+    model = keras.Model(inputs=img_inputs, outputs=output)
+    
+    return model
+
+"""
+Train given model using dataset
+"""
+def train_model(model):
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    history = model.fit(train.repeat(),
+              epochs=EPOCHS, 
+              steps_per_epoch=steps_per_epoch,
+              validation_data=validation.repeat(),
+              validation_steps=validation_steps)
+    
+    return history
+
+"""
+Evaluates given model
+"""
+def test_model(model):
+    test_loss, test_acc = model.evaluate(test)
+    print('Test accuracy:', test_acc)
+    print('Test loss:', test_loss)
+
+    predictions = model.predict(test)
+
+    return predictions
+
+# Load dataset from tensorflow dataset API
+SPLIT_WEIGHTS = (8, 1, 1)
+splits = tfds.Split.TRAIN.subsplit(weighted=SPLIT_WEIGHTS)
+(raw_train, raw_validation, raw_test), metadata = tfds.load(name="tf_flowers", 
+                                                            with_info=True,
+                                                            split=list(splits),                                                            
+                                                            as_supervised=True)
+
+# Format datasets
+train = raw_train.map(format_example)
+validation = raw_validation.map(format_example)
+test = raw_test.map(format_example)
+
+
+# Pre-process dataset
+train = train.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
+validation = validation.batch(BATCH_SIZE)
+test = test.batch(BATCH_SIZE)
+# (Optional) prefetch will enable the input pipeline to asynchronously fetch batches while
+# your model is training.
+train = train.prefetch(tf.data.experimental.AUTOTUNE)
+
+# Augment training dataset
+train = train.map(augment_data)
+
+# Calculating number of images in train, val and test sets
+# to establish a good batch size
+num_train, num_val, num_test = (
+metadata.splits['train'].num_examples * weight/10 
+for weight in SPLIT_WEIGHTS
+)
+steps_per_epoch = round(num_train) #BATCH_SIZE
+validation_steps = round(num_val) #BATCH_SIZE
+
+# Generate model
+model = create_model()
+
+# Train model and get training metrics
+history = train_model(model)
+
+# Evaluate model and get prediction metrics
+prediction = test_model(model)
